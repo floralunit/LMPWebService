1ea21cd9ab3e9edd1f1d8a4cac0409e045504033
floralunit <anya71034@gmail.com>
2025-04-19 19:09:42 +0300

    правки

--- Services/LeadsServiceJobScheduler.cs
+++ Services/LeadsServiceJobScheduler.cs
@@ -39,7 +39,7 @@ public class LeadsServiceJobScheduler : IHostedService
             .WithIdentity($"checkResponsibleJob", "groupCheckResponsibleJob")
             .StartNow()
             //.WithSimpleSchedule(x => x.WithInterval(TimeSpan.FromMinutes(30)).RepeatForever())
-            .WithCronSchedule("0 0 6-21 ? * * *", x => x.InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time")))
+            .WithCronSchedule("0 0/5 6-21 ? * * *", x => x.InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time")))
             .Build();
 
         await _scheduler.ScheduleJob(job1, trigger1, cancellationToken);
